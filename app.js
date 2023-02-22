bdae66f8873c498863659fe857febbc92d398b6f
Honvic7 <adewumivictor87@gmail.com>
2023-02-22 08:48:27 +0000

    app.my assignment

---
+++ app.js
@@ -0,0 +1,16 @@
+// var age = 20;
+// console.log(age);
+
+// const age = 25;
+// console.log(age);
+
+// let age = 30;
+// console.log(age);
+console.log(15 + 20 + 30);
+console.log(5 / 20);
+console.log(15 * 20 * 30);
+console.log(15 - 20);
+console.log(+'15' + 20 + 30);
+console.log(Math.random);
+console.log(Math.PI);
+console.log(parseInt('15') + 9);
